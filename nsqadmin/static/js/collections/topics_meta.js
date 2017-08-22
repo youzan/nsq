4586fd7d5621e1978bc4b718481d56fb57ef5fe5
元守 <lulin@youzan.com>
2017-08-22 19:49:47 +0800

    topic filters in nsqadmin
    Signed-off-by: 元守 <lulin@youzan.com>

--- nsqadmin/static/js/collections/topics_meta.js
+++ nsqadmin/static/js/collections/topics_meta.js
@@ -4,12 +4,21 @@ var Backbone = require('backbone');
 var AppState = require('../app_state');
 
 var Topics = require('./topics');
+var Topic = require('../models/topic');
 
-var TopicsMeta = Topics.extend({
+var TopicsMeta = Backbone.Collection.extend({
+    model: Topic,
+
+    comparator: 'id',
+
+    constructor: function TopicsMeta() {
+        Backbone.Collection.prototype.constructor.apply(this, arguments);
+    },
 
     url: function() {
         return AppState.url('/topics?metaInfo=true');
-    }
+    },
+
 });
 
 module.exports = TopicsMeta;
--- nsqadmin/static/js/views/topics.js
+++ nsqadmin/static/js/views/topics.js
@@ -4,55 +4,10 @@ var AppState = require('../app_state');
 var BaseView = require('./base');
 var Topics = require('../collections/topics');
 var TopicsMeta = require('../collections/topics_meta');
-var metaFetched = false;
 var TopicsView = BaseView.extend({
     className: 'topics container-fluid',
 
-    template: require('./spinner.hbs'),
-
-    events: {
-        'click .filters input': 'topicsFilterAction',
-    },
-
-    topicsFilterAction: function(e) {
-            e.stopPropagation();
-
-            if(!metaFetched) {
-                this.collection = new TopicsMeta();
-                this.collection.fetch()
-                    .done(function(data) {
-                        this.template = require('./topics.hbs');
-                        this.render({'message': data['message']});
-                        }.bind(this))
-                    .fail(this.handleViewError.bind(this))
-                    .always(Pubsub.trigger.bind(Pubsub, 'view:ready'));
-                metaFetched = true;
-                console.log("topic meta fetched.")
-            }
-
-            $('.table tr:not(.title)').show();
-
-            $(e.target).parent().find('input')
-                .filter(function(){
-                    if($(this).is(':checked')) {
-                        return this;
-                    }
-                })
-                .each(function() {
-                    var filterValue = $(this).val();
-                    $('.table tr:not(.title)')
-                        .filter(
-                            function(){
-                                if ($(this)
-                                    .find('td a#' + filterValue).length == 0){
-                                    return this;
-                                }
-                            }
-                        )
-                        .hide();
-                    }
-                );
-    },
+    template: require('./topics.hbs'),
 
     initialize: function() {
         BaseView.prototype.initialize.apply(this, arguments);
@@ -60,7 +15,6 @@ var TopicsView = BaseView.extend({
         this.collection = new Topics();
         this.collection.fetch()
             .done(function(data) {
-                this.template = require('./topics.hbs');
                 this.render({'message': data['message']});
             }.bind(this))
             .fail(this.handleViewError.bind(this))
