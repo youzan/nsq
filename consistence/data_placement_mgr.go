0cd29b177693f21b5f87d74447bf7577b7425f6e
Vincent Lee <cool8511@gmail.com>
2019-08-22 11:59:15 +0800

    adjust balance log and busy topic level

--- consistence/data_placement_mgr.go
+++ consistence/data_placement_mgr.go
@@ -71,7 +71,7 @@ const (
 	HIGHEST_PUB_QPS_LEVEL         = 100
 	HIGHEST_LEFT_CONSUME_MB_SIZE  = 50 * 1024
 	HIGHEST_LEFT_DATA_MB_SIZE     = 200 * 1024
-	busyTopicLevel                = 13
+	busyTopicLevel                = 15
 )
 
 type balanceOpLevel int
@@ -928,9 +928,6 @@ func (dpm *DataPlacement) balanceTopicLeaderBetweenNodes(monitorChan chan struct
 				sortedNodeTopicStats, moveOp, moveLeader)
 		}
 	}
-	if !checkMoveOK {
-		coordLog.Infof("check topic for moving can not move any on node: %v, all sorted topic: %v", statsMinMax[1].TopicHourlyPubDataList, sortedTopics)
-	}
 }
 
 func (dpm *DataPlacement) checkAndPrepareMove(monitorChan chan struct{}, fromNode string, topicName string, partitionID int,
@@ -943,7 +940,7 @@ func (dpm *DataPlacement) checkAndPrepareMove(monitorChan chan struct{}, fromNod
 	}
 	checkMoveOK := false
 	if topicInfo.OrderedMulti {
-		coordLog.Infof("topic %v is configured as multi ordered, no balance", topicName)
+		coordLog.Debugf("topic %v is configured as multi ordered, no balance", topicName)
 		return false
 	}
 	if moveOp > moveAny {
