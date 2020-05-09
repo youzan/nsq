269ceb0ab80d9d39b2eacbb9fa7e33ec766dff8c
Vincent Lee <cool8511@gmail.com>
2020-05-09 16:58:36 +0800

    optimize the consume latency to make sure each topic end flush will be notified to all channels

--- apps/nsq_tail/nsq_tail.go
+++ apps/nsq_tail/nsq_tail.go
@@ -51,7 +51,7 @@ func (th *TailHandler) HandleMessage(m *nsq.Message) error {
 	}
 	tn := time.Now().UnixNano()
 	if *showDelayed && tn >= m.Timestamp+time.Hour.Milliseconds()*500 {
-		os.Stdout.WriteString(fmt.Sprintf("delayed more than 500ms: %s %s\n", m.Timestamp, tn))
+		os.Stdout.WriteString(fmt.Sprintf("delayed more than 500ms: %v %v\n", m.Timestamp, tn))
 	}
 	if th.totalMessages > 0 && th.messagesShown >= th.totalMessages {
 		os.Exit(0)
